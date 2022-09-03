e6b4cfea8565cbe463c9ff5ea0d938f33c4ba944
kevkiiru <kevkiiru6@gmail.com>
2022-09-03 20:09:31 +0300

    the main c file

--- calendar/main.c
+++ calendar/main.c
@@ -39,18 +39,41 @@ void SetColor(int ForgC)//will set the forground color for printing
 
 	if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
 	{
-		wColor = (csbi.wAttributes & 0XF0) + (ForgC & 0XF0);
+		wColor = (csbi.wAttributes & 0XF0) + (ForgC & 0X0F);
 		SetConsoleTextAttribute(hStdOut, wColor);
 	}
 
 	return;
 }
 
+void ClearColor()
+{
+    SetColor(15);
+}
+
+void ClearConsoleToColors()
+{
+    WORD wColor = ((BackC & 0X0F) << 4) + (ForgC & 0X0F);
+
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD coord = {0, 0};
+
+    DWORD count;
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    SetConsoleTextAttribute(hStdOut, wColor);
+}
 
 
+/**
+ * main - main function
+ * Return: correct arithmetic
+ */
 
 
 int main()
 {
-
+	
 }
